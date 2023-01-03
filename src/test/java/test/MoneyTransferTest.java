580b0e1c61b03701b9826b394fd8c8a5bacec6c4
Alinaf <afassahova9@gmail.com>
2023-01-03 21:14:57 +0500

    add artefacts

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -1,9 +1,8 @@
 package test;
 
-import org.junit.jupiter.api.Test;
 import data.DataHelper;
+import org.junit.jupiter.api.Test;
 import page.LoginPage;
-
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,7 +11,6 @@ class MoneyTransferTest {
     void shouldTransferMoneyBetweenOwnCardsV1() {
         open("http://localhost:9999");
         var loginPage = new LoginPage();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
