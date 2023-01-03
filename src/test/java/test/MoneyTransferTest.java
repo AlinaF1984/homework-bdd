bf9facc51a962843e0463759092136c4fc442373
Alinaf <afassahova9@gmail.com>
2023-01-03 20:19:28 +0500

    initial commit

---
+++ src/test/java/test/MoneyTransferTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import data.DataHelper;
+import page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+}
